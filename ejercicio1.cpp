d150792829cd38ccec12bb62444bca6369ec4ce1
Alain Anthony Flores Tapia <alainloco9@gmail.com>
2025-05-02 10:25:34 -0400

    Update ejercicio1.cpp

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
 
+string getresult(string a, string b, string c);
+int main()
+{
+    // ingreso de las tres cadenas
+    string str1, str2, str3;
+cin >> str1 >> str2 >> str3;
+// calcular el resultado
+ string result = getresult(str1,str2,str3);
+ //muestra del resultado
+ cout<<result<<endl;
+    return 0;
+}
+
+    
+// para calcular cual es la cadena menor de las 3
+string getresult(string a,string b,string c){
+    string minimo= a;
+    if(b<minimo){
+        minimo=b;
+    }
+    if(c<minimo){
+        minimo=c;
+    }
+    return minimo;
+}
