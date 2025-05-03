d73055938feda0c3918cb0ca4894e50ffa51c6e6
Alain Anthony Flores Tapia <alainloco9@gmail.com>
2025-05-03 10:24:40 -0400

    Update ejercicio3.cpp

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
+void verific( int b);
+int calculate(int a , int b);
+void display (int result);
+    
 
+int main()
+
+{
+     int A,B;
+   cin>>A>>B;
+   verific(B);
+   int result = calculate(A, B);
+   display(result);
+   
+   
+   
+    return 0;
+}
+void verific(int b){
+    if(b==0){
+        cout<<"impossible"<<endl;
+    }
+}
+int calculate(int a , int b){
+    int cociente= a/b;
+    return cociente;
+}
+void display (int result){
+    cout<<"este es el cociente de la division "<<result<<endl;
+}
